0acf8edc2a5a2c9cb0a724be6b485e1951b0b670
Affan114 <affanshariff114@gmail.com>
2024-03-19 13:10:15 +0530

    indexOutOfBound

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        int [] arr = {12,12,345,687};
+        System.out.println(arr[4]);
     }
 }
