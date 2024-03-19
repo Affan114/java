38690181b7c3ff1866db7d371780b3dac42765b0
Affan114 <affanshariff114@gmail.com>
2024-03-19 13:22:06 +0530

    disadvantages of array is that data elements are to be homogeneous

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        int [] arr = {12,12,345,687};
-        System.out.println(arr[4]);
+        int [] arr = {12,12,345.23,687};
+        System.out.println(arr[3]);
     }
 }
