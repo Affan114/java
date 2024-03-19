7ea1916aa872e0440773a0791df5b0ec8373b29c
Affan114 <affanshariff114@gmail.com>
2024-03-19 14:37:21 +0530

    heap area outside string constant pool

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
 class HelloWorld {
     public static void main(String[] args) {
-      String s1 = "Only the dead have seen the end of war";
-      String s2 = "Only the dead have seen the end of war";
+      //heap area outside string constant pool
+
+      String s1 = new String("Only the dead have seen the end of war");
+      String s2 = new String("Only the dead have seen the end of war");
+
+      
       System.out.println(s1==s2);
     }
 }
