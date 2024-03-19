e2208fc12fc84be5748ec2ef9efbdffd17223d62
Affan114 <affanshariff114@gmail.com>
2024-03-19 13:44:58 +0530

    strings

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-        int [] arr = {12,12,345.23,687};
-        System.out.println(arr[3]);
+      // strings
+      String name = "Affan shariff";
+      System.out.println(name);
     }
 }
