ac2042d98c5b764aeea7ecc85bb36cc0b7ab6082
Affan114 <affanshariff114@gmail.com>
2024-03-19 14:33:43 +0530

    String constant pool proof

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-      // strings
-      String name = "Affan shariff";
-      System.out.println(name);
+      String s1 = "Only the dead have seen the end of war";
+      String s2 = "Only the dead have seen the end of war";
+      System.out.println(s1==s2);
     }
 }
