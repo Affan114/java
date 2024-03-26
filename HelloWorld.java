ba7a45b48c37274dd4e3ac1f4f30589cf122bb16
Affan114 <affanshariff114@gmail.com>
2024-03-26 16:38:05 +0530

    methods on strings

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,12 @@
 class HelloWorld {
-    public static void main(String[] args) {
-      String  s1 = "pw";
-      String s2 = "java";
-      String s3 = "pwjava";
-      String s4  = s1 + s2;
-      System.out.println(s3==s4);
+  public static void main(String[] args) {
 
+    String text = " Terra  Cota ";
+    System.out.println(text.toLowerCase());
+    System.out.println(text.toUpperCase());
+    System.out.println(text.strip());
+    System.out.println(text.charAt(2) );
+    System.out.println(text.trim());
 
-     
-    }
+  }
 }
-
-
