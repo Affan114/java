2fc8eaea57e154ac1715b4d6a550eee9a2b63c5b
Affan114 <affanshariff114@gmail.com>
2024-03-27 15:29:46 +0530

    classes in java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,11 @@
 class HelloWorld {
+  int x = 6;
   public static void main(String[] args) {
 
-    int result = sum(5,10);
-    System.out.println(result);
+   HelloWorld obj1 = new HelloWorld();
+   System.out.println(obj1.x);
 
 
   }
-  public static int sum(int start,int end ) {
-    if(end > start  ) {
-      return end + sum(start, end-1);
-    }
-    else {
-      return end;
-    }
-  }
+
 }
