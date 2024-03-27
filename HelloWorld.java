9ee08091e7821bb4fe704afd999725431e5d54ca
Affan114 <affanshariff114@gmail.com>
2024-03-27 15:17:40 +0530

    Recursion within a  range

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,17 @@
 class HelloWorld {
   public static void main(String[] args) {
 
-    int result = sum(100);
+    int result = sum(5,10);
     System.out.println(result);
 
 
   }
-  public static int sum(int n) {
-    if(n > 0 ) {
-      return n + sum(n-1);
+  public static int sum(int start,int end ) {
+    if(end > start  ) {
+      return end + sum(start, end-1);
     }
     else {
-      return 0;
+      return end;
     }
   }
 }
