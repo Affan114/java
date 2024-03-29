56eff59314c7910695000f75025f75fa785a00a0
Affan114 <affanshariff114@gmail.com>
2024-03-29 09:09:02 +0530

    String buffer

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,13 +2,9 @@ class HelloWorld {
   static int x ;
   public static void main(String[] args) {
 
-    String s1 = "Affan Shariff";
-    String s2 = " ";
-    String Sarr[] = s1.split(" ");
-    for(int i =  Sarr.length - 1; i>= 0 ; i--) {
-      s2 += Sarr[i];
-    }
-    System.out.println(s2);
+     StringBuffer sb = new StringBuffer("Affan ");
+     sb.append("Shariff");
+     System.out.println(sb);
   }
 
 }
