c43d39ffff686f177c6d3aeca075f87ec34983bc
Affan114 <affanshariff114@gmail.com>
2024-03-29 09:12:33 +0530

    string buffer cannot point to diff ref using Final

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld {
   static int x ;
   public static void main(String[] args) {
 
-     StringBuffer sb = new StringBuffer("Affan ");
+    final StringBuffer sb = new StringBuffer("Affan ");
      sb.append("Shariff");
     
      sb = new StringBuffer("Desire");
